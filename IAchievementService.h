8728240a98b725d86d8d90898da2f2e722bfebd1
honzapatCZ <honzapatCZ@email.cz>
2021-05-27 14:01:49 +0200

    string changes

--- IAchievementService.h
+++ IAchievementService.h
@@ -15,25 +15,25 @@ public:
 	/// <summary>
 	/// Sets achievemt of identifier and float value, NOTE: You are responsible for mapping this using something like config
 	/// </summary>
-	API_FUNCTION() virtual void SetAchievementProgress(StringView& identifier, float value) = 0;
+	API_FUNCTION() virtual void SetAchievementProgress(const String identifier, float value) = 0;
 
 	/// <summary>
 	/// Sets achievemt of identifier and float value, NOTE: You are responsible for mapping this using something like config
 	/// </summary>
-	API_FUNCTION() virtual void SetAchievement(StringView& identifier, bool value) = 0;
+	API_FUNCTION() virtual void SetAchievement(const String identifier, bool value) = 0;
 
 	/// <summary>
 	/// Gets all achievements, please return the universal or ignore the value if its not mapped
 	/// </summary>
-	//API_FUNCTION() virtual Array<MonoString*> GetAchievements() = 0;
+	API_FUNCTION() virtual Array<String> GetAchievements() = 0;
 
 	/// <summary>
 	/// Gets float value of achievement, NOTE: You are responsible for mapping this using something like config
 	/// </summary>
-	API_FUNCTION() virtual float GetAchievementProgress(StringView& identifier) = 0;
+	API_FUNCTION() virtual float GetAchievementProgress(const String identifier) = 0;
 
 	/// <summary>
 	/// Gets float value of achievement, NOTE: You are responsible for mapping this using something like config
 	/// </summary>
-	API_FUNCTION() virtual bool GetAchievement(StringView& identifier) = 0;
+	API_FUNCTION() virtual bool GetAchievement(const String identifier) = 0;
 };
